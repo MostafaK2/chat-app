9b309f5eb0aec99b14b2e7bcfd6c401c3258c165
Mostafa Kamal <mostafajunayed20@gmail.com>
2023-02-22 18:31:57 -0500

    added style for login backdrop

--- chat-app-frontend/src/components/pages/login/Registration.js
+++ chat-app-frontend/src/components/pages/login/Registration.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import classes from "./LogInPage.module.css";
 
 function Registration(props) {
   function cancelHandler() {
@@ -34,8 +35,9 @@ function Registration(props) {
   return (
     <div className="registration">
       <div>
-        <label>email</label>
         <input
+          placeholder="Set Email"
+          className={classes.input}
           type="email"
           id="username"
           onChange={(elem) => setUsername(elem.target.value)}
@@ -43,8 +45,9 @@ function Registration(props) {
       </div>
 
       <div>
-        <label>SetFirstName</label>
         <input
+          placeholder="Set First Name"
+          className={classes.input}
           type="first-name"
           id="fname"
           onChange={(elem) => setFname(elem.target.value)}
@@ -52,24 +55,30 @@ function Registration(props) {
       </div>
 
       <div>
-        <label>Set last Name</label>
+        
         <input
+          placeholder="Set last Name"
+          className={classes.input}
           type="last-name"
           id="lname"
           onChange={(elem) => setLname(elem.target.value)}
         ></input>
       </div>
 
-      <div>
-        <label>Set Password</label>
+      <div >
         <input
+          className={classes.input}
+          placeholder="Set Password"
           type="password"
           id="pass"
           onChange={(elem) => setPassword(elem.target.value)}
         ></input>
       </div>
-      <button onClick={cancelHandler}>Cancel</button>
-      <button onClick={confirmRegistration}>confirm</button>
+      <div className = {classes.buttonGrid}>
+        <button className ="btn" onClick={cancelHandler}>Cancel</button>
+        <button className="btn" onClick={confirmRegistration}>Confirm</button>
+      </div>
+     
     </div>
   );
 }
