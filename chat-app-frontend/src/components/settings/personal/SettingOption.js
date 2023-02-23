30d7efdbdb93643f20367ff96404619dd08fd7d0
Mostafa Kamal <mostafajunayed20@gmail.com>
2023-02-23 18:45:49 -0500

    added logout feature and start up code for landing page

--- chat-app-frontend/src/components/settings/personal/SettingOption.js
+++ chat-app-frontend/src/components/settings/personal/SettingOption.js
@@ -2,6 +2,7 @@ import classes from "../../conversations/ConversationItem.module.css";
 import sClasses from "./PersonalSettings.module.css";
 
 import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 
 function SettingOption(props) {
   const [clickedSection, setClickedSection] = useState(0);
@@ -13,17 +14,21 @@ function SettingOption(props) {
     return classes.colorChange;
   }
 
+  function onClick() {
+    setClickedSection(props.id);
+    if (props.id == 1) {
+      props.open();
+    }
+    else if (props.id === 6) {
+      localStorage.removeItem("jwt");
+      localStorage.removeItem("user");
+      window.location.replace('./login');
+    }
+    return props.callback(props.id);
+  }
+
   return (
-    <li
-      className={changeColor()}
-      onClick={() => {
-        setClickedSection(props.id);
-        if (props.id == 1) {
-          props.open();
-        }
-        return props.callback(props.id);
-      }}
-    >
+    <li className={changeColor()} onClick={onClick}>
       <div className={sClasses.container}>
         <img src={props.img} alt="" width="30" height="30"></img>
         <div>{props.name}</div>
