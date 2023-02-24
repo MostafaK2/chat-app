8ecad3e7db33e6cd1e1a865a54961eb9c79f969f
Mostafa Kamal <mostafajunayed20@gmail.com>
2023-02-24 12:56:30 -0500

    added css animation to preference menu

--- chat-app-frontend/src/components/settings/personal/ChangeNames.js
+++ chat-app-frontend/src/components/settings/personal/ChangeNames.js
@@ -2,36 +2,45 @@ import classes from "./ChangeNames.module.css";
 import { useState } from "react";
 
 function ChangeNames(props) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+ 
+
   // PUT update values
   function saveInformation() {
+    
+    if(firstName > 0 && lastName > 0){
+      
+    }
     props.close();
     return;
   }
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
+  
   return (
     <div className={classes.overlay}>
       <div className={classes.container}>
         <input
-		  className={classes.input}
+          className={classes.input}
           placeholder="New First Name"
           type="text"
           onChange={(elem) => setFirstName(elem.target.value)}
         ></input>
         <input
-		className={classes.input}
+          className={classes.input}
           placeholder="New Last Name"
           type="text"
           onChange={(elem) => setLastName(elem.target.value)}
         ></input>
 
         <div className={classes.child}>
-          <button className = {classes.button} onClick={() => props.close()}>Cancel</button>
-          <button className = {classes.button} onClick={saveInformation}>Save</button>
+          <button className={classes.button} onClick={() => props.close()}>
+            Cancel
+          </button>
+          <button className={classes.button} onClick={saveInformation}>
+            Save
+          </button>
         </div>
-
       </div>
     </div>
   );
--- chat-app-frontend/src/components/settings/personal/Preferences.js
+++ chat-app-frontend/src/components/settings/personal/Preferences.js
@@ -9,75 +9,79 @@ import ChangeNames from "./ChangeNames";
 import ChangePassword from "./ChangePassword";
 
 function Preferences() {
-//   const [clickedSection, setClickedSection] = useState(0);
+  //   const [clickedSection, setClickedSection] = useState(0);
 
-//   function changeColor() {
-//     if (clickedSection == props.clickedId) {
-//       return highlights.colorChangeAlt;
-//     }
-//     return highlights.colorChange;
-//   }
+  //   function changeColor() {
+  //     if (clickedSection == props.clickedId) {
+  //       return highlights.colorChangeAlt;
+  //     }
+  //     return highlights.colorChange;
+  //   }
 
-	const[changeNamesIsOpen, setChangeNameIsOpen] = useState(false);
-  const[passwordIsOpen, setPasswordIsOpen] = useState(false);
+  const [changeNamesIsOpen, setChangeNameIsOpen] = useState(false);
+  const [passwordIsOpen, setPasswordIsOpen] = useState(false);
 
-  function openChangeName(){
+  function openChangeName() {
     setChangeNameIsOpen(true);
     closePassword();
   }
 
-  function closeChangeName(){
+  function closeChangeName() {
     setChangeNameIsOpen(false);
   }
 
-  function openPassword(){
+  function openPassword() {
     setPasswordIsOpen(true);
     closeChangeName();
   }
-  function closePassword(){
+  function closePassword() {
     setPasswordIsOpen(false);
   }
 
   // animate after
-  function slide(){
-    if(changeNamesIsOpen || passwordIsOpen){
+  function slide() {
+    if (changeNamesIsOpen || passwordIsOpen) {
       return classes.boxalt;
     }
     return classes.box;
   }
 
   return (
-    <div className={slide()}>
-      <div className={classes.container}>
-        <h2>Preferences</h2>
+    <div>
+      <div className={slide()}>
+        <div className={classes.container}>
+          <h2>Preferences</h2>
 
-        <li className={highlights.colorChange}>
-          <div className={classes.child}>
-            <div className={classes.circle}></div>
-            <h4>Mostafa Kamal</h4>
-          </div>
-        </li>
+          <li className={highlights.colorChange}>
+            <div className={classes.child}>
+              <div className={classes.circle}></div>
+              <h4>Mostafa Kamal</h4>
+            </div>
+          </li>
 
-        {/* Changing Account Information */}
-        <li className={highlights.colorChange} onClick = {openChangeName}>
-          <div className={classes.child}>
-            <img src={nameImg} alt="" width={30} height={30} />
-            <h4>Change Account Information</h4>
-          </div>
-        </li>
+          {/* Changing Account Information */}
+          <li className={highlights.colorChange} onClick={openChangeName}>
+            <div className={classes.child}>
+              <img src={nameImg} alt="" width={30} height={30} />
+              <h4>Change Account Information</h4>
+            </div>
+          </li>
 
-        {/* changing password */}
-        <li className={highlights.colorChange} onClick={openPassword}>
-          <div className={classes.child}>
-            <img src={passImg} alt="" width={30} height={30} />
-            <h4>Change Password</h4>
-          </div>
-        </li>
+          {/* changing password */}
+          <li className={highlights.colorChange} onClick={openPassword}>
+            <div className={classes.child}>
+              <img src={passImg} alt="" width={30} height={30} />
+              <h4>Change Password</h4>
+            </div>
+          </li>
 
-        {/* Add notification and blocking features later */}
+          {/* Add notification and blocking features later */}
+        </div>
       </div>
-      {changeNamesIsOpen && <ChangeNames close={closeChangeName}/>}
-      {passwordIsOpen && <ChangePassword close={closePassword}/>}
+      <div>
+        {changeNamesIsOpen && <ChangeNames close={closeChangeName} />}
+        {passwordIsOpen && <ChangePassword close={closePassword} />}
+      </div>
     </div>
   );
 }
